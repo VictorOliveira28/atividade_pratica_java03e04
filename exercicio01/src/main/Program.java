fdb536fef77ccacb15fd7f6719fe3dda0973ea35
Victor Oliveira <victor.oliveira20@icloud.com>
2024-06-04 13:17:15 -0300

    exercise completed

--- exercicio01/src/main/Program.java
+++ exercicio01/src/main/Program.java
@@ -1,9 +1,30 @@
 package main;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		float salary;
+		float allowance;
+		float newSalary;
+		
+		System.out.println("Salário: ");
+		salary = sc.nextFloat();
+		
+		System.out.println("Abono: ");
+		allowance = sc.nextFloat();
+		
+		newSalary = salary + allowance;
+		
+		System.out.printf("Novo Salário: %.2f", newSalary);
+		
+		sc.close();
 	}
 
 }
