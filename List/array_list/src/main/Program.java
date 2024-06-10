82cb8bf9d42c1e808dfd207ebe2ef72ff763b308
Victor Oliveira <victor.oliveira20@icloud.com>
2024-06-10 13:25:44 -0300

    updated

--- List/array_list/src/main/Program.java
+++ List/array_list/src/main/Program.java
@@ -3,18 +3,23 @@ package main;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
 		List<String> list = new ArrayList<>();
 		
-		list.add("Azul");
-		list.add("Verde");
-		list.add("Amarelo");
-		list.add("Branco");
-		list.add("Laranja");
+		
+		System.out.println("Digite 5 cores a serem adicionadas: ");
+		
+		for(int i = 0; i < 5; i++) {
+			String color = sc.next();
+			list.add(color);
+		}
+		
 		
 		System.out.println("Listar todas as cores:");
 		for(String obj : list) {
@@ -32,6 +37,7 @@ public class Program {
 		}
 		
 		System.out.println(resultList.toString());
+		sc.close();
 	}
 
 }
